c5eb84f88ff3cdae4292ec61d72ab4dae30510ad
LadonAl <alaayoussef1997@gmail.com>
2019-09-27 22:58:23 +0400

    fix an issue with html files

--- public/javascript/navigation.js
+++ public/javascript/navigation.js
@@ -3,7 +3,7 @@
     let menuItems = document.getElementsByClassName("pure-menu-link");
     [...menuItems].forEach(element => {
         element.addEventListener('click', () => {
-            $("#content").load(element.id + ".html");
+            $("#content").load(element.id + ".html" + " #contents");
         });
     })
 })(this, this.document);
