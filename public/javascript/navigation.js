6c72f79cc06daed2c843c163f949c5f6f8a89047
LadonAl <alaayoussef1997@gmail.com>
2019-09-28 13:07:16 +0400

    grid and cards prototype

--- public/javascript/navigation.js
+++ public/javascript/navigation.js
@@ -1,10 +1,11 @@
 ((window, document) => {
 
-    let menuItems = document.getElementsByClassName("pure-menu-link");
-    [...menuItems].forEach(element => {
+    let menuItems = document.getElementsByClassName("pure-menu-link");  // getting all menu links   
+    [...menuItems].forEach(element => {  // using menu items(html collection) as array 
         element.addEventListener('click', () => {
-            $("#content").load(element.id + ".html" + " #contents");
-            $("#pageTitle").html(element.id.replace(/^\w/, c => c.toUpperCase()));
+            $("#content").load(element.id + ".html" + " #contents");//adding an event listener to each menu link that changes the 
+                                                                    //contents of the content div to be (%id%.html)
+            $("#pageTitle").html(element.id.replace(/^\w/, c => c.toUpperCase()));  //changing the page title to that id (first letter capitalized)
         });
     })
 })(this, this.document);
