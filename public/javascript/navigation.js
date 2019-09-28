1732a2764ece4d1e71e75e9633dae3ec51fe3ee4
LadonAl <alaayoussef1997@gmail.com>
2019-09-28 10:14:10 +0400

    '/' automatically loads home Page title changes with menue clicks

--- public/javascript/navigation.js
+++ public/javascript/navigation.js
@@ -4,6 +4,7 @@
     [...menuItems].forEach(element => {
         element.addEventListener('click', () => {
             $("#content").load(element.id + ".html" + " #contents");
+            $("#pageTitle").html(element.id.replace(/^\w/, c => c.toUpperCase()));
         });
     })
 })(this, this.document);
