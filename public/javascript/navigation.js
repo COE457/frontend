7c32e279e378b137ff968b2f2320bdbb2fbc3af4
LadonAl <alaayoussef1997@gmail.com>
2019-10-04 14:34:18 +0400

    fixed an issue with login check

--- public/javascript/navigation.js
+++ public/javascript/navigation.js
@@ -16,66 +16,61 @@
   //  wrapping the script in a function to avoid global variables
   //DUMMY LOGIN CHECK
   //To be replaced with authentication check when the API is ready
-  if (localStorage.length === 0 /*@todo replace with a valid condition*/) {
-    // if the user is NOT logged in
-    window.location.href = "/login"; // redirect to the login page
-  } else {
-    // if a valid
-    let menuItems = document.getElementsByClassName("menuLink"); //  getting all menu links
-    [...menuItems].forEach(element => {
-      //using menu items(html collection) as array
-      element.addEventListener("click", () => {
-        if (element.id == "logout") {
-          //if the user selects "Logout"
-          localStorage.clear(); //  clear local storage
-          document.location.reload(); //  reload the page
-        } else {
-          console.log(element.id);
-          $("#content").load(element.id + ".html" + " #contents", () => {
-            //different actions based on selection
-            switch (element.id) {
-              case "home":
-                //@todo add functions and API calls to get actual data
-                break;
 
-              case "status":
-                /**
-                 * @function t() <= thermometer.js
-                 * @function h() <= heartRate.js
-                 */
-                t("#thermometerRoom"); //  load room thermometer data @todo add API call to get actual reading
-                t("#thermometerBody"); //  load body thermometer data @todo add API call to get actual reading
-                h(); //  load heart rate data @todo add API call to get actual reading
-                //@todo add functions and API calls to get actual data
-                break;
+  let menuItems = document.getElementsByClassName("menuLink"); //  getting all menu links
+  [...menuItems].forEach(element => {
+    //using menu items(html collection) as array
+    element.addEventListener("click", () => {
+      if (element.id == "logout") {
+        //if the user selects "Logout"
+        localStorage.clear(); //  clear local storage
+        document.location.reload(); //  reload the page
+      } else {
+        console.log(element.id);
+        $("#content").load(element.id + ".html" + " #contents", () => {
+          //different actions based on selection
+          switch (element.id) {
+            case "home":
+              //@todo add functions and API calls to get actual data
+              break;
 
-              case "location":
-                /** @function updateLocation() <= location.js */
-                updateLocation(); // generate locations table @todo add API call to get actual reading
-                break;
+            case "status":
+              /**
+               * @function t() <= thermometer.js
+               * @function h() <= heartRate.js
+               */
+              t("#thermometerRoom"); //  load room thermometer data @todo add API call to get actual reading
+              t("#thermometerBody"); //  load body thermometer data @todo add API call to get actual reading
+              h(); //  load heart rate data @todo add API call to get actual reading
+              //@todo add functions and API calls to get actual data
+              break;
 
-              case "objects":
-                /** @function updateObjects() <= object.js */
-                updateObjects(); // generate objects table @todo add API call to get actual reading
-                break;
+            case "location":
+              /** @function updateLocation() <= location.js */
+              updateLocation(); // generate locations table @todo add API call to get actual reading
+              break;
 
-              case "communication":
-                /** @function attachCommunicationFunctions() <= communication.js */
-                attachCommunicationFunctions();
-                break;
+            case "objects":
+              /** @function updateObjects() <= object.js */
+              updateObjects(); // generate objects table @todo add API call to get actual reading
+              break;
 
-              default:
-                break;
-            }
-          });
+            case "communication":
+              /** @function attachCommunicationFunctions() <= communication.js */
+              attachCommunicationFunctions();
+              break;
 
-          /**
-           * adding an event listener to each menu link that changes the
-           * contents of the content div to be (%id%.html)
-           */
-          $("#pageTitle").html(element.id.replace(/^\w/, c => c.toUpperCase())); //changing the page title to that id (first letter capitalized)
-        }
-      });
+            default:
+              break;
+          }
+        });
+
+        /**
+         * adding an event listener to each menu link that changes the
+         * contents of the content div to be (%id%.html)
+         */
+        $("#pageTitle").html(element.id.replace(/^\w/, c => c.toUpperCase())); //changing the page title to that id (first letter capitalized)
+      }
     });
-  }
+  });
 })(this, this.document); //  calling the function
