a1fe774fc4b8dd825d051a548f945e01459b0154
Alaa Youssef <ladonal@pop-os.localdomain>
2019-11-14 12:32:36 +0400

    add smartwatch now functional

--- public/javascript/manageSmartwatch.js
+++ public/javascript/manageSmartwatch.js
@@ -1,16 +1,51 @@
 ((window, document) => {
-    $("#addChild").click(e => {
-        $("#modal").load("addChild.html #form", () => {
-            showModal();
-            $("#leftButton").click(e => {});
-            $("#rightButton").click(e => {
-                e.preventDefault();
-                hideModal();
+  $("#addChild").click(e => {
+    $("#modal").load("addChild.html #form", () => {
+      showModal();
+      $("#leftButton").click(e => {
+        $.ajax({
+          url: "http://localhost:3001/API/child/create",
+          dataType: "json",
+          type: "post",
+          contentType: "application/json",
+          data: JSON.stringify({
+            name: $("#childName").val(),
+            Parent: localStorage.getItem("userID")
+          }),
+          processData: false,
+          success: function(data, textStatus, jQxhr) {
+            console.log("Child added successfully, adding smartwatch...");
+            console.log(data);
+            $.ajax({
+              url: "http://localhost:3001/API/smartwatch/create",
+              dataType: "json",
+              type: "post",
+              contentType: "application/json",
+              data: JSON.stringify({
+                serialNumber: $("#smartWatchSN").val(),
+                Child: data.id
+              }),
+              success: function(data, textStatus, jQxhr) {
+                console.log("Smartwatch added successfully. Refreshing...");
+                console.log(data);
+                window.location.href = "/";
+              },
+              error: function(jqXhr, textStatus, errorThrown) {
+                console.log(errorThrown);
+              }
             });
+          },
+          error: function(jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+          }
         });
+      });
+      $("#rightButton").click(e => {
+        e.preventDefault();
+        hideModal();
+      });
     });
+  });
 
-    $("#removeChild").click(e => {
-        
-    });
+  $("#removeChild").click(e => {});
 })(this.window, this.document);
