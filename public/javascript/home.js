692250ff715597867b0bd99309b8bf810b18e876
Amna Altunaiji <>
2019-11-16 16:33:55 +0400

    ++equipt --mentalStatus

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -2,3 +2,18 @@ const updateLocationIcon = () => {
     let latestLocation = JSON.parse(localStorage.getItem("latestLocation"));
     $("#latestLocation").html(latestLocation.location.join());
 }
+const equipt = () =>{
+    $.ajax({
+    url: "http://192.168.137.1:3001/API/equipmentHistory/read",
+    dataType: "json",
+    type: "get",
+    contentType: "application/json",
+    data: { Smartwatch: localStorage.getItem("currentSmartwatch") },
+    processData: false,
+    success: data =>
+        {
+             let equipt = data.docs[data.docs.length - 1].equipped;
+           $("#equiptt").html((equipt)?"Equipt":"Not Equipt");
+        }
+    
+})};
