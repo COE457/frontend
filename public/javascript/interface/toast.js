1ed6650ddac7b80bf43e3c4190be3ad8fdb32da6
Alaa Youssef <ladonal@pop-os.localdomain>
2019-11-26 17:07:30 +0400

    added clear option to notifications

--- public/javascript/interface/toast.js
+++ public/javascript/interface/toast.js
@@ -1,23 +1,32 @@
+let timeOuts = [];
 const newToast = contents => {
   let currentNotifications = Number($("#badge").html());
   $("#badge").html(currentNotifications + 1);
   let newToast = $("<div>", { class: "toast" }).html(`<p>${contents}</p>`);
   $("#toastContainer").append(newToast);
-  newToast.animate({ right: "+=100%" }, 350, "linear").click(e => {
-    currentNotifications = Number($("#badge").html());
-    $("#badge").html(currentNotifications - 1);
-    newToast.remove();
-  });
-  setTimeout(() => {
-    newToast.animate({ right: "-=100%" }, 350, "linear", () => {
-      newToast.hide();
+  newToast
+    .show()
+    .animate({ right: "+=100%" }, 350, "linear")
+    .click(e => {
+      currentNotifications = Number($("#badge").html());
+      $("#badge").html(currentNotifications - 1);
+      newToast.remove();
     });
-  }, 5000);
+  timeOuts.push(
+    setTimeout(() => {
+      newToast.animate({ right: "-=100%" }, 350, "linear", () => {
+        newToast.hide();
+      });
+    }, 5000)
+  );
 };
 
 $("#notification").click(e => {
   let display = $(".toast").css("display");
-
+  timeOuts.forEach(item => {
+    clearTimeout(item);
+  });
+  $(".toast").css({"right": "-100%"});
   setTimeout(() => {
     if (display == "block") {
       $(".toast").animate({ right: "-=100%" }, 350, "linear", () => {
@@ -29,4 +38,12 @@ $("#notification").click(e => {
         .animate({ right: "+=100%" }, 350, "linear");
     }
   }, 100);
+});
+
+$("#clearNot").click(e => {
+  $(".toast:not(.option)").remove();
+  $("#badge").html(0);
+  $(".toast").animate({ right: "-=100%" }, 350, "linear", () => {
+    $(".toast").hide();
+  });
 });
