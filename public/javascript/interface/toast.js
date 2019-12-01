82226fedf9952374bf6490c410da129c1c045099
ladonal <alaayoussef1997@gmail.com>
2019-12-01 05:11:11 +0400

    clean toast animation

--- public/javascript/interface/toast.js
+++ public/javascript/interface/toast.js
@@ -1,54 +1,58 @@
 let timeOuts = [];
+
+const showToast = toast => {
+  toast.css({ right: "-100%" });
+  toast
+    .show()
+    .animate({ right: "+=100%" }, 350, "linear");
+}
+
+const hideToast = toast => {
+  toast.animate({ right: "-=100%" }, 350, "linear", () => {
+    toast.hide();
+  });
+}
+
+const deleteToast = toast => {
+  currentNotifications = Number($("#badge").html());
+  $("#badge").html(currentNotifications - 1);
+  toast.animate({ right: "+=100%" }, 150, "linear", () => {
+    toast.remove();
+  });
+}
+
 const newToast = contents => {
   let currentNotifications = Number($("#badge").html());
   $("#badge").html(currentNotifications + 1);
   let newToast = $("<div>", { class: "toast" }).html(`<p>${contents}</p>`);
-  $("#toastContainer").append(newToast);
-  newToast
-    .show()
-    .animate({ right: "+=100%" }, 350, "linear")
-    .click(e => {
-      currentNotifications = Number($("#badge").html());
-      $("#badge").html(currentNotifications - 1);
-      newToast.animate({ right: "+=100%" }, 350, "linear", () => {
-        newToast.remove();
-      });
-    });
+  $("#toastBody").prepend(newToast);
+  newToast.on(`click ${events.clearNotifications}`, e => {
+    deleteToast(newToast);
+  });
+  showToast(newToast);
   timeOuts.push(
     setTimeout(() => {
-      newToast.animate({ right: "-=100%" }, 350, "linear", () => {
-        newToast.hide();
-      });
-    }, 5000)
+      hideToast(newToast);
+    }, 3000)
   );
 };
 
 $("#notification").click(e => {
   let display = $(".toast").css("display");
+
   timeOuts.forEach(item => {
     clearTimeout(item);
   });
-  $(".toast").css({ right: "-100%" });
-  setTimeout(() => {
-    if (display == "block") {
-      $(".toast").animate({ right: "-=100%" }, 350, "linear", () => {
-        $(".toast").hide();
-      });
-    } else {
-      $(".toast")
-        .show()
-        .animate({ right: "+=100%" }, 350, "linear");
-    }
-  }, 100);
+
+
+  if (display == "block") {
+    hideToast($(".toast"));
+  } else {
+    showToast($(".toast"));
+  }
 });
 
 $("#clearNot").click(e => {
-  $(".toast").animate({ right: "+=100%" }, 350, "linear", () => {
-    $(".toast").hide();
-    $(".toast:not(.option)").remove();
-    $("#badge").html(0);
-    $(".toast").animate({ right: "-=100%" }, 350, "linear", () => {
-      $(".toast").hide();
-    });
-  });
+  $(".toast").trigger(events.clearNotifications);
+  hideToast($(".toast"));
 });
--- public/javascript/util/events.js
+++ public/javascript/util/events.js
@@ -10,5 +10,7 @@
  const events = {
      viewReady: "viewReady", //  when #contents is done loading 
      refreshed: "refreshed", //  when the page is refreshed and done loading
-     locationUpdated: "locationUpdated" //  when location data gets updated 
+     locationUpdated: "locationUpdated", //  when location data gets updated 
+     newLocation: "newLocation", //  when a new location is detected
+     clearNotifications: "clearNotifications" //  when notifications should be cleared
  }
